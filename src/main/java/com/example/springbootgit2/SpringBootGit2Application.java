ea6f56bd4835b6600730d4f19557146e56a97720
abebe <abebetekle@gmail.com>
2021-06-25 15:34:30 -0500

    Add all my files

---
+++ src/main/java/com/example/springbootgit2/SpringBootGit2Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootgit2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGit2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGit2Application.class, args);
+    }
+
+}
